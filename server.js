23e8f53a08cc6b9609b18a47b5565542c1457484
Mohit-S-Rwt <122927518+Mohit-S-Rwt@users.noreply.github.com>
2023-08-24 21:57:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoute.js"
 import cors from 'cors';
 import path from 'path';
-
+import {fileURLToPath} from 'url';
 
 // configure env;
 dotenv.config();
@@ -16,6 +16,9 @@ dotenv.config();
 
 // connect db
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //  rest object
 const app = express();
